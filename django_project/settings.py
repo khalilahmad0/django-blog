8bd43d1923f4c3787c44b57de7af023776f7886d
Khalil Al-Ahmad <kia09@mail.aub.edu>
2020-06-05 15:34:37 +0300

    fixed debug value

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '95bap(i$jq6&tw+!7%8=14u4yx(mt@-w933usa__r#f8!+tl#p'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['django-microblog-00.herokuapp.com']
 
