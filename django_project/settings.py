e018caae17651664c2829869b6f1e1d4ad59c115
Khalil Al-Ahmad <kia09@mail.aub.edu>
2020-06-05 15:15:47 +0300

    Added secret key

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-microblog-00.herokuapp.com/']
+ALLOWED_HOSTS = ['django-microblog-00.herokuapp.com']
 
 
 # Application definition
